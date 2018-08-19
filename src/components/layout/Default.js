2920eb1021645241ee6329f6db9e07829e045d2f
Nathan Carlson <nathan.s.carlson@gmail.com>
2018-08-18 22:01:25 -0600

    Remove unneeded packages

--- src/components/layout/Default.js
+++ src/components/layout/Default.js
@@ -1,34 +1,6 @@
-// @flow
-
 import React, { Fragment, Component } from 'react'
-import type { Location } from 'react-router'
 
-
-type Props = {
-  location: Location,
-
-}
-
-class Default extends Component<Props> {
-  componentDidMount() {
-    window.addEventListener('keydown', this.kbShortcut)
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.location !== prevProps.location) {
-      const canScroll =
-        this._scrollContainer &&
-        this._scrollContainer._scrollbar &&
-        this._scrollContainer._scrollbar.scrollTo
-      if (canScroll) {
-        // $FlowFixMe already checked this._scrollContainer
-        this._scrollContainer._scrollbar.scrollTo(0, 0)
-      }
-    }
-  }
-
-  _scrollContainer = null
-
+class Default extends Component {
   render() {
     return (
         <Fragment>
